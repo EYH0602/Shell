d4be12a383cde0bb07ab5786d93e79c771e47a7f
Ethan He <yfhe@ucdavis.edu>
2021-04-19 16:27:56 +0800

    use -Wall -Werror

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ wish: wish.cpp
 	$(CC) -o wish wish.cpp $(FLAGS)
 
 new: wish_new.cpp
-	$(CC) -o wish wish_new.cpp 
+	$(CC) -o wish wish_new.cpp $(FLAGS)
 
 passed: wish_passed.c
 	gcc -o wish wish_passed.c $(FLAGS)
--- wish.cpp
+++ wish.cpp
@@ -14,331 +14,403 @@
 #include <unistd.h>
 #include <wait.h>
 
-#define BUFF_SIZE 4096
+#define BUFF_SIZE 200
+#define EMPTY_COMMAND = -1
 
 using namespace std;
 
-/*** ERROR ***/
-void throw_error() {
-    char error_message[30] = "An error has occurred\n";
+// * system settings * //
+const char* bin = "/bin";
+const char* usrbin = "/usr/bin";
+char* paths[BUFF_SIZE] = {(char*)bin, (char*)usrbin};  // default search path
+int paths_len = 2;
+
+void throw_error(int code) {
+    char error_message[30] = "An error has occurred\n\0";
     write(STDERR_FILENO, error_message, strlen(error_message)); 
-}
-
-/*** build-in commands ***/
-vector<string> paths = {"/bin/", "/usr/bin/"};
-int my_exit() {
-    exit(0);
-}
-
-// chdir()
-// 0 or >1 args -> error
-int my_cd(char** argv) {
-    if (!argv[1]) {
-        throw_error();
-        return 1;
+    if (code != 0 && code != 1) {
+        return;
     }
-    // try to chdir
-    if (chdir(argv[1]) != 0) {
-        throw_error();
-        return 1;
+    exit(code);
+}
+
+bool is_buildin(char* command) {
+    return strcmp(command, "exit") == 0 ||
+           strcmp(command, "cd") == 0 ||
+           strcmp(command, "path") == 0 ||
+           strcmp(command, "PATH") == 0;
+}
+
+// * string operations * //
+char* trim(char* str) {
+    // form head
+    while (isspace(*str) && *str) {
+        str++;
     }
-    return 0;   // success
-}
 
-int my_path(vector<string> paths_in) {
-    paths_in.erase(paths_in.begin());
-    paths = paths_in;
-    // for (int i = 1; argv[i] != NULL; i++) {
-    //     new_paths.push_back(string(argv[i]));
-    // }
-    return 0;
-}
-
-// testing
-int PATH() {
-    for (string path: paths) {
-        string path_with_newline = path + '\n';
-        write(STDOUT_FILENO, path_with_newline.c_str(), path_with_newline.length());
+    // if reach, return
+    if (!*str) {
+        return str;
     }
-    return 0;
-}
 
-
-
-
-/*** functions acting on commands ***/
-// unix API requires c-style string
-bool is_command_char(char ch) {
-    return isalpha(ch) || 
-           isdigit(ch) ||
-           ch == '-' || 
-           ch == '/' || 
-           ch == '.' || 
-           ch == '>' || 
-           ch == '!' ||
-           ch == '?';
-}
-
-string trim(string s) {
-    int len = s.length();
-    // trim left
-    while (!is_command_char(s[0]) && len > 0) {
-        s = s.substr(1);
-        len--;
+    // from tail, put a '\0' after the last nonspace char
+    char* end = str + strlen(str) - 1;
+    while (isspace(*end) && end != str) {
+        end--;
     }
-    // trim right
-    while (!is_command_char(s[len-1]) && len > 0) {
-        s.pop_back();
-        len--;
-    }
-    return s;
+
+    *(end + 1) = '\0';
+    return str;
 }
 
-vector<string> parse_line(char* command, char delimiter, char end) {
-    string line = "";
-    vector<string> tokens;
-    bool hasBegin = false;
+// command structure
+struct Command{
+    int argc;       // number of arguments in this command
+    int fd_out;     // file descripter for stdout
+    int fd_err;     // file descripter for stderr
+    char path[BUFF_SIZE];   // path to command
+    char* argv[BUFF_SIZE];  // argument vector of this command
 
-    for (char* ch = command; *ch; ch++) {
-        if (!hasBegin && *ch == ' ')
-            continue;
-        hasBegin = true;
-        line += *ch;
-        if (*ch == delimiter || *ch == '\n' || *ch == '\0') {
-            tokens.push_back(trim(line) + end);
-            line = "";
-        }
+    Command() {
+        this->argc = 0;
+        this->fd_out = -1;
+        this->fd_err = -1;
+        memset(this->path, 0, sizeof(this->path));
+        memset(this->argv, 0, sizeof(this->argv));
     }
-    return tokens;
-}
 
-char** parse_command(char* command, bool is_buildin) {
-    vector<string> tokens = parse_line(command, ' ', '\0'); 
-    int len = tokens.size();
-    char** argv = new char*[len];
-    for (int i = 0; i < len; i++) {
-        char* comm = (char*)tokens[i].c_str();
-        comm[tokens[i].length()] = '\0';
-        argv[i] = comm;
-    }
-    if (is_buildin)
-        argv[len] = NULL;
-    return argv;
-}
+    Command(char* line) {
+        char* separated[BUFF_SIZE] = {};    // seperated line by > redirection
+        int count = 0;                      // count of >, at most 1
+        int can_access;
+        bool is_empty_here = false;
 
-char* find_path(char* command){
-    for (size_t i = 0; i < paths.size(); i++) {
-        char* buff = new char[paths[i].length() + 1];
-        strcpy(buff, paths[i].c_str());
-        // string path_with_end = paths[i] + '/';
-        strcat(buff, command);
-        if (access(buff, X_OK) == 0) {
-            return buff;
-        }
-        delete buff; 
-    }
-    // cerr << "wish: command not found: " << command << endl;
-    throw_error();
-    exit(1);
-}
-
-string first(string sentence) {
-    string res = "";
-    for (char ch : sentence) {
-        if (ch == ' ' || ch == '\n') {
-            break;
-        }
-        res += ch;
-    }
-    return res;
-}
-
-int fd = STDOUT_FILENO;
-
-int update_file_descriptor(string& command) {
-    vector<string> words;
-    string comm_new = "";
-    int count = 0;
-    bool hasBegin = false;
-
-    for (char ch : command) {
-        if (!hasBegin && ch == ' ')
-            continue;
-        hasBegin = true;
-        if (ch == '>') {
-            if (comm_new != "")
-                words.push_back(trim(comm_new));
-            words.push_back(">");
-            comm_new = "";
+        
+        // ! check for redirection
+        while ((separated[count] = strsep(&line, ">")) != NULL) {
             count++;
-            continue;
         }
-        comm_new += ch;
-        if (ch == '\n' || ch == '\0') {
-            if (comm_new != "\n" && comm_new != "\0" && comm_new != " ") {
-                words.push_back(trim(comm_new));
+        count--;    // the above loop will add 1 extra
+        if (count > 1) {
+            throw_error(2);
+        } else if (count == 0) {  // no redirection
+            
+            this->fd_out = STDOUT_FILENO;
+            this->fd_err = STDERR_FILENO;
+        } else {
+            if (strcmp(separated[0], "") == 0 || strcmp(separated[1], "") == 0) {
+                // throw_error(2);
+                is_empty_here = true;
             }
-            comm_new = "";
-        }
-    }
-    // check valid syntax
-    if (count > 1) {
-        throw_error(); 
-        exit(0);
-    }
-    // for (size_t i = 0; i < words.size(); i++) {
-    //     cout << i << ": " << words[i] << endl;
-    // }
 
-    // cout << words.size() << endl;
 
-    comm_new = "";
-    for (size_t i = 0; i < words.size(); i++) {
-        // cout << words[i] << "    " << (words[i] == ">") << endl;
-        if (words[i] == ">") {
-            if (i+1 >= words.size() || i == 0) {
-                throw_error();
-                exit(0);
-            }
-            if (words[i+1].find(" ") != string::npos) {
-                throw_error();
-                exit(0);
+            separated[1] = trim(separated[1]); // redirection target location
+
+            // redirection only support 1 file,
+            // if there is space in target location after trim
+            // it means there are more than 1 file location
+            if (string(separated[1]).find(" ") != string::npos) {
+                // throw_error(0);
+                is_empty_here = true;
             }
 
             mode_t S_MODE = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
-            int O_FLAG = O_CREAT | O_RDWR;
-            fd = open(words[i+1].c_str(), O_FLAG, S_MODE);
-            if (fd < 0) {
-                throw_error();
-                return 1;
+            int O_FLAG = O_CREAT | O_RDWR | O_TRUNC;
+            int fd;
+
+            if ((fd = open(separated[1], O_FLAG, S_MODE)) < 0) {
+                // throw_error(1);
+                is_empty_here = true;
             }
 
-            dup2(fd,1);
-            dup2(fd,2);
-            close(fd);
-            break;
+            this->fd_out = fd;
+            this->fd_err = fd;
         }
-        comm_new += words[i];
+
+
+        // ! read in the command
+        this->argc = 0;                  // count of argument
+        separated[0] = trim(separated[0]);  // command string
+
+        while ((this->argv[this->argc] = strsep(&separated[0], " ")) != NULL) {
+            this->argc++;
+        }
+
+        // cout << this->argc << endl;
+
+        // trip all
+        for (int i = 0; i < this->argc; i++) {
+            this->argv[i] = trim(this->argv[i]);
+            // cout << this->argv[i] << endl;
+        }
+
+        // skip if build-in command
+        if (is_buildin(this->argv[0])){
+            strcpy(this->path, this->argv[0]);
+        } else {
+            // cout << "here" << endl;
+            for (int i = 0; i < paths_len; i++) {
+                this->path[0] = '\0';   // reset buffer
+                // printf("!!!-> %d\n", i);
+                // for (int i = 0; i < paths_len; i++) {
+                //     cout << paths[i] << endl;
+                // }
+                // printf("<- !!!\n");
+                strcpy(this->path, paths[i]);
+                strcat(this->path, "/");
+                strcat(this->path, this->argv[0]);
+                // cout << this->path << endl;
+                can_access = access(this->path, X_OK);
+                if (can_access == 0) {
+                    break;
+                }
+                if (can_access == -1 && i == paths_len-1) {
+                    is_empty_here = true;
+                }
+            }
+        }
+
+        if (is_empty_here){
+            this->path[0] = '\0';   // reset buffer
+            strcpy(this->path, this->argv[0]);
+            // use -1 tp denote empty command
+            this->fd_out = -1;
+            this->fd_err = -1;
+        }
+
     }
-    command = comm_new + '\n';
-    return 0;
+};
+
+bool is_empty_command(Command com) {
+    return com.fd_out == -1 || com.fd_err == -1;
 }
 
-int apply_command(char* line) {
-    // parse the input line
-    pid_t ret;
-    vector<string> commands = parse_line(line, '&', '\n');
-    // for (string cm : commands) {
-    //     cout << "=>" << cm << "<=" << endl;
-    // }
-    
-
-    // check if it is build-in command
-    // change to function pointer if time allows
-    if (first(commands[0]) == "exit") {
-        if (commands[0] != "exit\n") {
-            throw_error();
-            exit(0);
+bool has_empty(Command comms[], int len) {
+    for (int i = 0; i < len; i++) {
+        if (is_empty_command(comms[i])) {
+            return true;
         }
-        my_exit();
     }
-    if (first(commands[0]) == "cd")
-        return my_cd(parse_command((char*)commands[0].c_str(), true));
-    if (first(commands[0]) == "path")
-        return my_path(parse_line((char*)commands[0].c_str(), ' ', '/'));
-    if (first(commands[0]) == "PATH")
-        return PATH();
+    return false;
+}
 
+// const Command EMPTY_COMMAND = Command();
+
+// * build-in commands * //
+void my_cd(int argc, char** argv) {
+    if (argc != 2) {
+        throw_error(0);
+    }
+    if (chdir(argv[1]) != 0) {
+        throw_error(1);
+    }
+}
+
+void my_path(int argc, char** argv) {
+    // no arguments, reset search path to empty
+    if (argc == 1) {
+        memset(&paths[0], 0, sizeof(paths));
+        paths_len = 0;
+    }
+    paths_len = 0;
+    for (int i = 1; i < argc; i++) {
+        paths[i-1] = strdup(argv[i]);
+        paths_len += 1;
+    }
+}
+
+void my_exit(int argc, char** argv) {
+    if (argc != 1) {
+        throw_error(0);
+    }
+    exit(0);
+}
+
+void PATH() {
+    for (int i = 0; i < paths_len; i++) {
+        cout << paths[i] << endl;
+    }
+}
+
+void close_all_fd(Command commands[], int len) {
+    for (int i = 0; i < len; i++) {
+        if (commands[i].fd_out != STDOUT_FILENO) {
+            close(commands[i].fd_out);
+        }
+        if (commands[i].fd_err != STDERR_FILENO) {
+            close(commands[i].fd_err);
+        }
+    }
+}
+
+#define SHOW_PROMPT write(STDOUT_FILENO, prompt, strlen(prompt))
+
+int apply_command(Command commands[], int len) {
+    pid_t pids[BUFF_SIZE];
     
-    // external commands
-    for (int i = 0; i < (int)commands.size(); i++) {
-        // apply the command
-        ret = fork();
-        if (ret < 0) {
-            throw_error();
-            return 1;
-        } else if (ret == 0) {
-            // child process,
-            // excute the command here
-            
-            // check if there is redirection
-            update_file_descriptor(commands[i]);
-            char** argv = parse_command((char*)commands[i].c_str(), false);
-            char* path = find_path(argv[0]);
-            strcpy(argv[0], path);
-            for (int i = 0; argv[i] != NULL; i++) {
-                cout << ">>" << argv[i] << "<<" << endl;
+
+    for (int i = 0; i < len; i++) {
+        pids[i] = fork();
+        if (pids[i] < 0) {
+            throw_error(1);
+        } else if (pids[i] == 0) {
+            // child process
+            // execute the command here
+            if (is_empty_command(commands[i])) {
+                throw_error(1);
             }
-            execv(path, argv);
-            delete path;
-            delete argv;
-            exit(0);
-        } else {
-            // parent process,
-            // wait until the child process finish
+
+            // int out = dup(fileno(stdout));
+            // int err = dup(fileno(stderr));
+            
+            dup2(commands[i].fd_out, 1);
+            dup2(commands[i].fd_err, 2);
+
+            // if (commands[i].fd_out != STDOUT_FILENO) {
+            //     char emp[BUFF_SIZE];
+            //     memset(emp, 0, sizeof(emp));
+            //     write(commands[i].fd_out, emp, BUFF_SIZE);
+            // }
+
+            // if (dup2(commands[i].fd_out, 1) == -1) {
+            //     return errno;
+            // }
+            // if (dup2(commands[i].fd_err, 2) == -1) {
+            //     return errno;
+            // }
+
+
+            // cout << commands[i].path << "  " << commands[i].argv[0] << endl;
+            execv(commands[i].path, commands[i].argv);
+
+
+
+            // fflush(stdout);
+            // fflush(stderr);
+            close(commands[i].fd_out);
+            close(commands[i].fd_err);
+            // dup2(out, fileno(stdout));
+            // dup2(err, fileno(stderr));
+            // close(out);
+            // close(err);
+            // exit(errno);
+        } 
+        else {
             wait(NULL);
         }
     }
-
+    // for (int i = 0; i < len; i++) {
+    //     waitpid(pids[i], &status, 0);
+    // }
     return 0;
 }
 
-/*** main program ***/
-int start_shell(FILE* fp) {
-    size_t len;
-    // int rnt;
-    char* buff = NULL;
-    char prompt[] = "wish> ";
-    bool hasStarted = false;
+void printCommand(Command command) {
+    cout << "argc:  "   << command.argc << endl;;      // number of arguments in this command
+    cout << "fd_out:    " << command.fd_out << endl;;    // file descripter for stdout
+    cout << "fd_err:    " << command.fd_err << endl;;    // file descripter for stderr
+    cout << "path:  " << command.path << endl;;   // path to command
+    cout << "argv:" << endl;
+    for (int i = 0; i < command.argc; i++)
+        cout << "   " << i << ": " << command.argv[i] << endl;
+    cout << "==========" << endl;
+}
 
-    // read from file line by line
-    while (1) {
-        // prompt if interactive mode
-        if (fp == stdin)
-            write(STDOUT_FILENO, prompt, 6);
-        // read in from stdin or file, stop if EOF
-        if(getline(&buff, &len, fp) == EOF) {
-            // cout << "EOF" << endl;
-            //  exit(1);
-            if (!hasStarted) {
-                throw_error();
-                return 1;
-            }
-            return 0;
-        }
-        if (len == 0) {
-            throw_error();
-            return 1;
-            // cout << 0 << " len" << endl;
-        }
-        hasStarted = true;
-        // apply the command and get the return value
-        buff[len] = '\0';
-        apply_command(buff);
+// * main program * //
+int start_shell(FILE* fp) {
+
+    size_t cap = 0;
+    ssize_t len;
+    char* buff = NULL;
+
+    // prompt for interactive mode
+    char prompt[] = "wish> ";
+    if (fp != stdin) {
+        prompt[0] = '\0';
     }
-    // otherwise the commands are applied successfully
-    return 0;
+
+
+    // // read from file/STDIN line by line
+    while (SHOW_PROMPT, len = getline(&buff, &cap, fp) > 0) {
+        buff = trim(buff); // move pointer to the first nonspace char 
+
+        // skip empty line
+        if (strcmp(buff, "") == 0) {
+            continue;
+        }
+        // cout << "[][][] " << buff << endl;
+
+        Command commands[BUFF_SIZE];
+        char* command_buff[BUFF_SIZE];
+        int count = 0;
+
+        // separate commands with &
+        while ((command_buff[count] = strsep(&buff, "&")) != NULL) {
+            count++;
+        }
+
+        // cout << "<<<" << count << ">>>" << endl;
+
+        // store the commands in Command struct and in array
+        for (int i = 0; i < count; i++) {
+            commands[i] = Command(command_buff[i]);
+            // printCommand(commands[i]);
+        }
+
+
+
+        if (count == 1) {
+            // check if it is build in command
+            if (strcmp(commands[0].argv[0], "exit") == 0) {
+                my_exit(commands[0].argc, commands[0].argv);
+                continue;
+            }
+            if (strcmp(commands[0].argv[0], "path") == 0) {
+                my_path(commands[0].argc, commands[0].argv);
+                continue;
+            }
+            if (strcmp(commands[0].argv[0], "cd") == 0) {
+                my_cd(commands[0].argc, commands[0].argv);
+                continue;
+                return 0;
+            }
+            if (strcmp(commands[0].argv[0], "PATH") == 0) {
+                PATH();
+                continue;
+            }
+
+
+            // not a buildin command
+            if (paths_len == 0) {
+                throw_error(2);
+            }
+            if (apply_command(commands, count) != 0) {
+                throw_error(1);
+            }
+
+        } else {
+            if (apply_command(commands, count) != 0) {
+                throw_error(1);
+            }
+        }
+
+        // close_all_fd(commands, count);
+    }
+    // fclose(fp);
+    return 0; 
 }
 
 int main(int argc, char* argv[]) {
-    int rnt;
-
-    // mode
-    //  argc == 1 -> interactive mode, read from stdin
-    //  otherwise -> batch mode, read in the batch file
+    if (argc > 2) {
+        throw_error(1);
+    }
     FILE* fp = (argc == 1) ? stdin : fopen(argv[1], "r");
 
+
     if (!fp) {
-        throw_error();
-        return 1;
+        throw_error(1);
     }
-
-    rnt = start_shell(fp);
-
+    int rnt = start_shell(fp);
     return rnt;
 }
-
-
-
-
 
